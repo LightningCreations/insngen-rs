d9a514d28f2dbd8cdfe3e1dd73eccd08eaf11104
Connor <chorman64@gmail.com>
2021-04-16 23:26:56 -0400

    Remove basically useless lifetime bounds

--- insngen-core/src/lex.rs
+++ insngen-core/src/lex.rs
@@ -1,23 +1,10 @@
-use std::{
-    borrow::Cow,
-    iter::{FromIterator, FusedIterator},
-};
+use std::iter::{FromIterator, FusedIterator};
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum Literal<'a> {
-    String(Cow<'a, str>),
+pub enum Literal {
+    String(String),
     Character(char),
     Integer(i128),
-}
-
-impl<'a> Literal<'a> {
-    pub fn clone_to_static(&self) -> Literal<'static> {
-        match self {
-            Self::String(c) => Literal::String(Cow::Owned(c.to_string())),
-            Self::Character(c) => Literal::Character(*c),
-            Self::Integer(i) => Literal::Integer(*i),
-        }
-    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
@@ -28,106 +15,72 @@ pub enum Delimeter {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum TokenTree<'a> {
-    Ident(Cow<'a, str>),
-    Lit(Literal<'a>),
-    Delimeted(Delimeter, TokenStream<'a>),
+pub enum TokenTree {
+    Ident(String),
+    Lit(Literal),
+    Delimeted(Delimeter, TokenStream),
     Punct(char),
 }
 
-impl<'a> TokenTree<'a> {
-    pub fn clone_to_static(&self) -> TokenTree<'static> {
-        match self {
-            Self::Ident(c) => TokenTree::Ident(Cow::Owned(c.to_string())),
-            Self::Lit(l) => TokenTree::Lit(l.clone_to_static()),
-            Self::Delimeted(d, strm) => TokenTree::Delimeted(*d, strm.clone_to_static()),
-            Self::Punct(c) => TokenTree::Punct(*c),
-        }
-    }
-}
-
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct TokenStream<'a> {
-    stream: Vec<TokenTree<'a>>,
+pub struct TokenStream {
+    stream: Vec<TokenTree>,
 }
 
-impl<'a> TokenStream<'a> {
-    pub fn clone_to_static(&self) -> TokenStream<'static> {
-        TokenStream {
-            stream: self.stream.iter().map(TokenTree::clone_to_static).collect(),
-        }
-    }
-
-    pub fn iter(&self) -> Iter<'_, 'a> {
+impl TokenStream {
+    pub fn iter(&self) -> Iter<'_> {
         Iter(self.stream.iter())
     }
 }
 
-impl<'a> IntoIterator for TokenStream<'a> {
-    type IntoIter = IntoIter<'a>;
-    type Item = TokenTree<'a>;
-    fn into_iter(self) -> IntoIter<'a> {
+impl IntoIterator for TokenStream {
+    type IntoIter = IntoIter;
+    type Item = TokenTree;
+    fn into_iter(self) -> IntoIter {
         IntoIter(self.stream.into_iter())
     }
 }
 
-impl<'a, 'b: 'a> IntoIterator for &'a TokenStream<'b> {
-    type IntoIter = Iter<'a, 'b>;
-    type Item = &'a TokenTree<'b>;
-    fn into_iter(self) -> Iter<'a, 'b> {
+impl<'a> IntoIterator for &'a TokenStream {
+    type IntoIter = Iter<'a>;
+    type Item = &'a TokenTree;
+    fn into_iter(self) -> Iter<'a> {
         self.iter()
     }
 }
 
-impl<'a, 'b: 'a> FromIterator<TokenTree<'b>> for TokenStream<'a> {
-    fn from_iter<I: IntoIterator<Item = TokenTree<'b>>>(it: I) -> Self {
+impl FromIterator<TokenTree> for TokenStream {
+    fn from_iter<I: IntoIterator<Item = TokenTree>>(it: I) -> Self {
         Self {
             stream: it.into_iter().collect(),
         }
     }
 }
 
-impl<'a, 'b: 'a> FromIterator<TokenStream<'b>> for TokenStream<'a> {
-    fn from_iter<I: IntoIterator<Item = TokenStream<'b>>>(it: I) -> Self {
+impl FromIterator<TokenStream> for TokenStream {
+    fn from_iter<I: IntoIterator<Item = TokenStream>>(it: I) -> Self {
         Self {
             stream: it.into_iter().flatten().collect(),
         }
     }
 }
 
-impl<'a> Extend<TokenTree<'a>> for TokenStream<'a> {
-    fn extend<I: IntoIterator<Item = TokenTree<'a>>>(&mut self, iter: I) {
+impl Extend<TokenTree> for TokenStream {
+    fn extend<I: IntoIterator<Item = TokenTree>>(&mut self, iter: I) {
         self.stream.extend(iter)
     }
 }
 
-impl<'a> Extend<TokenStream<'a>> for TokenStream<'a> {
-    fn extend<I: IntoIterator<Item = TokenStream<'a>>>(&mut self, iter: I) {
+impl Extend<TokenStream> for TokenStream {
+    fn extend<I: IntoIterator<Item = TokenStream>>(&mut self, iter: I) {
         self.stream.extend(iter.into_iter().flatten())
     }
 }
 
-pub struct Iter<'a, 'b: 'a>(std::slice::Iter<'a, TokenTree<'b>>);
+pub struct Iter<'a>(std::slice::Iter<'a, TokenTree>);
 
-impl<'a, 'b: 'a> Iterator for Iter<'a, 'b> {
-    type Item = &'a TokenTree<'b>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next()
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.0.size_hint()
-    }
-}
-
-impl<'a, 'b: 'a> FusedIterator for Iter<'a, 'b> {}
-impl<'a, 'b: 'a> ExactSizeIterator for Iter<'a, 'b> {}
-
-pub struct IntoIter<'a>(std::vec::IntoIter<TokenTree<'a>>);
-
-impl<'a> Iterator for IntoIter<'a> {
-    type Item = TokenTree<'a>;
+impl<'a> Iterator for Iter<'a> {
+    type Item = &'a TokenTree;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.0.next()
@@ -138,5 +91,22 @@ impl<'a> Iterator for IntoIter<'a> {
     }
 }
 
-impl<'a> FusedIterator for IntoIter<'a> {}
-impl<'a> ExactSizeIterator for IntoIter<'a> {}
+impl<'a> FusedIterator for Iter<'a> {}
+impl<'a> ExactSizeIterator for Iter<'a> {}
+
+pub struct IntoIter(std::vec::IntoIter<TokenTree>);
+
+impl<'a> Iterator for IntoIter {
+    type Item = TokenTree;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl FusedIterator for IntoIter {}
+impl ExactSizeIterator for IntoIter {}
